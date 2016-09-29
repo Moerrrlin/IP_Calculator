09e6e2a5d85c89a8e0d58a2949fe6d2158246525
Merlin Rübsteck <merlin@myrdinr.de>
2016-09-29 11:48:46 +0200

    Added function which can check if a given vlue is a real binary number. Moved leadingZero funtion to the Binary class

--- src/calc/Binary.java
+++ src/calc/Binary.java
@@ -1,22 +1,40 @@
+package calc;
+
 public class Binary {
     private String value;
 
-    public void setValue(String s) {
-        value = s;
+    public void setValue(String s, int b) {
+        value = leadingZero(s, b);
     }
-    public void setValue(int i){
-        value = Integer.ToBinaryString(i);
+    public void setValue(int i, int b){
+        value = leadingZero(Integer.toBinaryString(i), b);
     }
     public String getValue() {
         return value;
     }
+    public Boolean ifBinary(String s){
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)){
+                case '0':   break;
+                case '1':   break;
+                default:    return false;
+            }
+        }
+        return true;
+    }
+    public String leadingZero(String s, int b) {
+		while (s.length() < b) {
+			s = '0' + s;
+		}
+		return s;
+	}
     public void Binary() {
-        value = '0';
+        value = "0";
     }
     public void Binary(String s) {
         value = s;
     }
     public void Binary(int i){
-        value = Integer.ToBinaryString(i);
+        value = Integer.toBinaryString(i);
     }
 }
