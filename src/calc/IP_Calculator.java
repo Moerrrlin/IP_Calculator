6f857c849fd610aef21a20fd0b32984f70d5a861
it5kjk <jnkiepert@gmail.com>
2016-10-18 16:45:36 +0200

    added arbitrary option menu

--- src/calc/IP_Calculator.java
+++ src/calc/IP_Calculator.java
@@ -1,34 +1,57 @@
 package calc;
 
+import java.util.Scanner;
 
 public class IP_Calculator {
 
 	public static void main(String[] args) {
+		Scanner menu = new Scanner(System.in);
+		// arbitrary option menu
+		System.out.println(
+				"*******************************\n".concat(
+				"*                             *\n").concat(		
+				"*       IPv4 Calculator       *\n").concat(
+				"*                             *\n").concat(
+				"*******************************\n").concat(
+				"*           Options           *\n").concat(
+				"* (1) decimal input to binary *\n").concat(
+				"*                             *\n").concat(
+				"* (2) binary input to decimal *\n").concat(		
+				"*******************************\n")
+		);
 		
-		IPv4_Header header = new IPv4_Header();
+		int option = menu.nextInt();
+		if (option == 1){
+				menu.close();
+				IPv4_Header header = new IPv4_Header();
+				Validator valD = new Validator(header);
+				
+				// fetch user input for header information
+				valD.setVersion();
+				
+				valD.setIhl();
+				
+				valD.setTos();
+				
+				valD.setID();
+				
+				valD.setFlag();
 
-		Validator valD = new Validator(header);
+				valD.setFragment_offset();
+				
+				valD.setTtl();
+				
+				valD.setProtocol();
+				
+				valD.setSourceIp();
+				
+				valD.setDestinationIp();
+		} else if (option == 2) {
+			menu.close();
+			Binary header = new Binary();
+//			BinaryValidator bVal = new BinaryValidator(header);
+		}
 		
-		valD.setVersion();
-		
-		valD.setIhl();
-		
-		valD.setTos();
-		
-		valD.setID();
-		
-		valD.setFlag();
-
-		valD.setFragment_offset();
-		
-		valD.setTtl();
-		
-		valD.setProtocol();
-		
-		valD.setSourceIp();
-		
-		valD.setDestinationIp();
-
 		/*
 		header.setHeader(); // set the header via user input
 
