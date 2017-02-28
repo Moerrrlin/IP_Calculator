455027cf0f5d319c66db7d3c6c357f8616c00f3d
Merlin Rübsteck <merlin@myrdinr.de>
2017-02-28 13:28:27 +0100

    Addedbinary option to the IPv6 Address class. The implementation is not finished nor working

--- src/calc/IPv6_Address.java
+++ src/calc/IPv6_Address.java
@@ -1,32 +1,52 @@
 package calc;
 
 public class IPv6_Address{
-    private String[] address = new String[8];
+    private String[] address_hex = new String[8];
+    private String address_bin = "";
 
-    public void setAddress(String[] addr){
-        address = addr;
+    public void setAddressHex(String[] addr){
+        address_hex = addr;
     }
 
-    public String[] getAddress(){
-        return address;
+    public void setAddressBin(String addr){
+        address_bin = addr;
     }
 
-    public String getAddress(int i){
-        return address[i];
+    public String[] getAddressHex(){
+        return address_hex;
     }
 
-    public String toString(){
+    public String getAddressHex(int i){
+        return address_hex[i];
+    }
+
+    public String getAddressBin(){
+        return address_bin;
+    }
+
+    public String toStringHex(){
         String string = "";
         for(int i = 0; i < 8; i++){
-            string += getAddress(i);
+            string += getAddressHex(i);
         }
         return string;
     }
 
-    public void display(){
+    public void displayHex(){
         String string = "";
         for(int i = 0; i < 8; i++){
-            string += getAddress(i);
+            string += getAddressHex(i);
+            if(i < 8 - 1){
+                string += ":";
+            }
+        }
+        System.out.println(string);
+    }
+
+    public void displayBin(){
+        String string = "";
+        for(int i = 0; i < 8; i++){
+            string += getAddressBin(i);
             if(i < 8 - 1){
                 string += ":";
             }
@@ -38,7 +58,7 @@ public class IPv6_Address{
         String hex = "0123456789ABCDEF";
         String addressBinary = "";
         for(int i = 0; i < 8; i++){
-            String addressElement = this.getAddress(i).toUpperCase();
+            String addressElement = this.getAddressHex(i).toUpperCase();
             for(int j = 0; j < 4; j++){
                 char addressElementChar = addressElement.charAt(j);
                 Binary addressElementBinary = new Binary(Integer.toBinaryString(hex.indexOf(addressElementChar)));
@@ -49,7 +69,13 @@ public class IPv6_Address{
         return addressBinary;
     }
 
+    public string toHexadecimalString(){
+        String addressHexadecimal = "";
+
+        return addressHexadecimal;
+    }
+
     public IPv6_Address(String[] addr){
-        address = addr;
+        address_hex = addr;
     }
 }
