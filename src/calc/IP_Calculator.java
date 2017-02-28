91ca26a9a64988c941a7253e3ae89c2c1cb77540
Merlin Rübsteck <merlin@myrdinr.de>
2017-02-28 12:51:13 +0100

    Added IPv6 to the Menu. Options are still TODO

--- src/calc/IP_Calculator.java
+++ src/calc/IP_Calculator.java
@@ -10,14 +10,24 @@ public class IP_Calculator {
 		// arbitrary option menu
 		System.out.println(
 				"*******************************\n" +
-				"*                             *\n" +		
+				"*                             *\n" +
 				"*       IPv4 Calculator       *\n" +
 				"*                             *\n" +
 				"*******************************\n" +
 				"*           Options           *\n" +
 				"* (1) decimal input to binary *\n" +
 				"*                             *\n" +
-				"* (2) binary input to decimal *\n" +		
+				"* (2) binary input to decimal *\n" +
+				"*                             *\n" +
+				"*******************************\n" +
+				"*                             *\n" +
+				"*       IPv6 Calculator       *\n" +
+				"*                             *\n" +
+				"*******************************\n" +
+				"*           Options           *\n" +
+				"* (3) decimal input to binary *\n" +
+				"*                             *\n" +
+				"* (4) binary input to decimal *\n" +
 				"*******************************\n");
 		while (!valid_option) {
 			System.out.print("Enter option: ");
@@ -27,38 +37,38 @@ public class IP_Calculator {
 				System.out.println(">>Preparing to convert header information. Please enter the following data to proceed.");
 				IPv4_Header header = new IPv4_Header();
 				Validator valD = new Validator(header);
-				
+
 				// fetch user input for header information
 				valD.setVersion();
-				
+
 				valD.setIhl();
-				
+
 				valD.setTos();
-				
+
 				valD.setTotalLength();
-				
+
 				valD.setID();
-				
+
 				valD.setFlag();
 
 				valD.setFragmentOffset();
-				
+
 				valD.setTtl();
-				
+
 				valD.setProtocol();
 
 				System.out.println(">> The checksum is handled as 0. "
 					+ "Computing will be initialized after packet is complete.");
-				
+
 				valD.setSourceIp();
-				
+
 				valD.setDestinationIp();
-				
+
 				valD.computeChecksum();
-				
+
 				header.print();
 				header.printBinary();
-				
+
 				//close the scanner
 				menu.close();
 			} else if (option == 2) {
@@ -69,6 +79,12 @@ public class IP_Calculator {
 				System.out.print("\nDecimal header information:\n"
 				+ bHeader.toDecimalHeaderString());
 				menu.close();
+			} else if (option == 3) {
+				valid_option = true;
+				System.out.println("TODO");
+			} else if (option == 4) {
+				valid_option = true;
+				System.out.println("TODO");
 			} else {
 				System.out.println(">>No such option!");
 			}
