fc8a910c83d23e8fe59acdc499eb6a37063ed1b2
Merlin Rübsteck <merlin@myrdinr.de>
2017-03-21 11:10:28 +0100

    Changed if else to a switch

--- src/calc/IP_Calculator.java
+++ src/calc/IP_Calculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class IP_Calculator {
 
 	public static void main(String[] args) {
-		/*Scanner menu = new Scanner(System.in);
+		Scanner menu = new Scanner(System.in);
 		boolean valid_option = false;
 		// arbitrary option menu
 		System.out.println(
@@ -28,67 +28,67 @@ public class IP_Calculator {
 				"* (3) decimal input to binary *\n" +
 				"*                             *\n" +
 				"* (4) binary input to decimal *\n" +
+				"*                             *\n" +
 				"*******************************\n");
 		while (!valid_option) {
 			System.out.print("Enter option: ");
 			int option = menu.nextInt();
-			if (option == 1){
-				valid_option = true;
-				System.out.println(">>Preparing to convert header information. Please enter the following data to proceed.");
-				IPv4_Header header = new IPv4_Header();
-				Validator valD = new Validator(header);
+			switch(option){
+				case 1:
+					valid_option = true;
+					/*
+					System.out.println(">>Preparing to convert header information. Please enter the following data to proceed.");
+					IPv4_Header header = new IPv4_Header();
+					Validator valD = new Validator(header);
 
-				// fetch user input for header information
-				valD.setVersion();
-
-				valD.setIhl();
-
-				valD.setTos();
-
-				valD.setTotalLength();
-
-				valD.setID();
-
-				valD.setFlag();
-
-				valD.setFragmentOffset();
-
-				valD.setTtl();
-
-				valD.setProtocol();
-
-				System.out.println(">> The checksum is handled as 0. "
+					// fetch user input for header information
+					valD.setVersion();
+					valD.setIhl();
+					valD.setTos();
+					valD.setTotalLength();
+					valD.setID();
+					valD.setFlag();
+					valD.setFragmentOffset();
+					valD.setTtl();
+					valD.setProtocol();
+					
+					System.out.println(">> The checksum is handled as 0. "
 					+ "Computing will be initialized after packet is complete.");
-
-				valD.setSourceIp();
-
-				valD.setDestinationIp();
-
-				valD.computeChecksum();
-
-				header.print();
-				header.printBinary();
-
-				//close the scanner
-				menu.close();
-			} else if (option == 2) {
-				valid_option = true;
-				
-				System.out.println(">>Preparing to convert header information.\n"
+					
+					valD.setSourceIp();
+					valD.setDestinationIp();
+					valD.computeChecksum();
+					
+					header.print();
+					header.printBinary();
+					*/
+					//close the scanner
+					menu.close();
+					break;
+				case 2:
+					valid_option = true;
+					/*
+					System.out.println(">>Preparing to convert header information.\n"
 						+ ">>Please enter a binary header object to proceed:");
-				Binary bHeader = new Binary(Validator.fetchUserInput("Input:"));
-				System.out.print("\nDecimal header information:\n"
-				+ bHeader.toDecimalHeaderString());
-				menu.close();
-			} else if (option == 3) {
-				valid_option = true;
-				System.out.println("TODO");
-			} else if (option == 4) {
-				valid_option = true;
-				System.out.println("TODO");
-			} else {
-				System.out.println(">>No such option!");
+					Binary bHeader = new Binary(Validator.fetchUserInput("Input:"));
+					System.out.print("\nDecimal header information:\n"
+					+ bHeader.toDecimalHeaderString());
+					*/
+					menu.close();
+					break;
+				case 3:
+					valid_option = true;
+					IPv6_Validator validator = new IPv6_Validator();
+					menu.close();
+					break;
+				case 4:
+					valid_option = true;
+					System.out.println("TODO");
+					menu.close();
+					break;
+				default:
+					System.out.println(">>No such option!");
 			}
-		}*/
+		}
 	}
 }
